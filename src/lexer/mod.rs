0db8a37192a2042ca39513f5192dbc7d3d295437
John Szaniszlo <Johnszaniszlo@gmail.com>
2025-01-27 20:29:56 -0800

    lex symbols and tests

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -21,6 +21,9 @@ pub enum LexerError {
 
     #[error("Malformed integer")]
     MalformedInteger,
+
+    #[error("Invalid symbol")]
+    InvalidSymbol,
 }
 
 pub fn lex_identifier(src: &str) -> Result<(Token, usize), LexerError> {
@@ -77,11 +80,17 @@ pub fn lex_number(src: &str) -> Result<(Token, usize), LexerError> {
                 found_dot = true;
                 Ok(true)
             }
+        // don't let it absorb symbols or whitespace and fail.
+        } else if ch.is_ascii_punctuation() || ch.is_whitespace() {
+            Ok(false)
+        // let everything else be aborbed for error cases.
+        // (stuff like 123a or 123bar, should fail.)
         } else {
             Ok(true)
         }
     })?;
 
+    // allowing it to absorb alphabetic chars in the last step allows for easy fail cases here.
     if found_dot {
         let float  = match number.parse::<f64>() {
             Ok(f) => Ok(f),
@@ -96,6 +105,36 @@ pub fn lex_number(src: &str) -> Result<(Token, usize), LexerError> {
         Ok((Token::from(integer), read_bytes))
     }
 }
+
+fn lex_symbol(src: &str) -> Result<(Token, usize), LexerError> {
+    let (symbol, read_bytes) = consume_while(src, |ch| Ok(ch.is_ascii_punctuation()))?;
+
+    match symbol {
+        "=" => Ok((Token::Equals, read_bytes)),
+        ":=" => Ok((Token::InferredEquals, read_bytes)),
+        "==" => Ok((Token::EqualsEquals, read_bytes)),
+        "!=" => Ok((Token::NotEquals, read_bytes)),
+        "&&" => Ok((Token::And, read_bytes)),
+        "||" => Ok((Token::Or, read_bytes)),
+        ":" => Ok((Token::Colon, read_bytes)),
+        "::" => Ok((Token::ColonColon, read_bytes)),
+        "=>" => Ok((Token::FatArrow, read_bytes)),
+        "," => Ok((Token::Comma, read_bytes)),
+        "(" => Ok((Token::LeftParen, read_bytes)),
+        ")" => Ok((Token::RightParen, read_bytes)),
+        "{" => Ok((Token::LeftBrace, read_bytes)),
+        "}" => Ok((Token::RightBrace, read_bytes)),
+        "[" => Ok((Token::LeftBracket, read_bytes)),
+        "]" => Ok((Token::RightBracket, read_bytes)),
+        "|" => Ok((Token::Union, read_bytes)),
+        "+" => Ok((Token::Plus, read_bytes)),
+        "-" => Ok((Token::Minus, read_bytes)),
+        "/" => Ok((Token::Divide, read_bytes)),
+        "*" => Ok((Token::Multiply, read_bytes)),
+        _ => Err(LexerError::InvalidSymbol),
+    }
+}
+
 
 fn consume_while<F>(src: &str, mut pred: F) -> Result<(&str, usize), LexerError>
     where F: FnMut(char) -> Result<bool, LexerError> {
@@ -160,3 +199,26 @@ lexer_test!(FAIL:lex_malformed_float3, lex_number, "5.a44f");
 lexer_test!(FAIL:lex_malformed_integer, lex_number, "0123");
 lexer_test!(FAIL:lex_malformed_integer2, lex_number, "0000123");
 lexer_test!(FAIL:lex_malformed_integer3, lex_number, "1a23");
+
+lexer_test!(lex_equals, lex_symbol, "=" => Token::Equals);
+lexer_test!(lex_infered_equals, lex_symbol, ":=" => Token::InferredEquals);
+lexer_test!(lex_equals_equals, lex_symbol, "==" => Token::EqualsEquals);
+lexer_test!(lex_not_equals, lex_symbol, "!=" => Token::NotEquals);
+lexer_test!(lex_and, lex_symbol, "&&" => Token::And);
+lexer_test!(lex_or, lex_symbol, "||" => Token::Or);
+lexer_test!(lex_colon, lex_symbol, ":" => Token::Colon);
+lexer_test!(lex_colon_colon, lex_symbol, "::" => Token::ColonColon);
+lexer_test!(lex_fat_arrow, lex_symbol, "=>" => Token::FatArrow);
+lexer_test!(lex_comma, lex_symbol, "," => Token::Comma);
+lexer_test!(lex_left_paren, lex_symbol, "(" => Token::LeftParen);
+lexer_test!(lex_right_paren, lex_symbol, ")" => Token::RightParen);
+lexer_test!(lex_left_brace, lex_symbol, "{" => Token::LeftBrace);
+lexer_test!(lex_right_brace, lex_symbol, "}" => Token::RightBrace);
+lexer_test!(lex_left_bracket, lex_symbol, "[" => Token::LeftBracket);
+lexer_test!(lex_right_bracket, lex_symbol, "]" => Token::RightBracket);
+lexer_test!(lex_union, lex_symbol, "|" => Token::Union);
+lexer_test!(lex_plus, lex_symbol, "+" => Token::Plus);
+lexer_test!(lex_minus, lex_symbol, "-" => Token::Minus);
+lexer_test!(lex_divide, lex_symbol, "/" => Token::Divide);
+lexer_test!(lex_multiply, lex_symbol, "*" => Token::Multiply);
+
