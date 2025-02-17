80a4d36124a6cf3fcff3b1e04158eec231c44409
Edward Eckelberry <edwardeckelberry@Edwards-MacBook-Air.local>
2025-02-17 11:45:41 -0800

    test code init

---
+++ src/lexer/tests.rs
@@ -0,0 +1,14 @@
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_example() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn test_another_example() {
+        assert!(true);
+    }
+}
